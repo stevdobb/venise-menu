7433645be31b9b8b164fb6ab6412f2244062da53
Steven Dobbelaere <steven@streamovations.be>
2026-04-03 09:27:02 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -18,52 +18,52 @@ export default defineConfig({
             theme_color: '#0f5eb6',
             "icons": [
               {
-                "src": "/venise-menu/icons/icon-48x48.png",
+                "src": "/venise-menu/pwa/icon-48x48.png",
                 "sizes": "48x48",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-72x72.png",
+                "src": "/venise-menu/pwa/icon-72x72.png",
                 "sizes": "72x72",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-96x96.png",
+                "src": "/venise-menu/pwa/icon-96x96.png",
                 "sizes": "96x96",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-128x128.png",
+                "src": "/venise-menu/pwa/icon-128x128.png",
                 "sizes": "128x128",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-144x144.png",
+                "src": "/venise-menu/pwa/icon-144x144.png",
                 "sizes": "144x144",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-152x152.png",
+                "src": "/venise-menu/pwa/icon-152x152.png",
                 "sizes": "152x152",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-192x192.png",
+                "src": "/venise-menu/pwa/icon-192x192.png",
                 "sizes": "192x192",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-256x256.png",
+                "src": "/venise-menu/pwa/icon-256x256.png",
                 "sizes": "256x256",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-384x384.png",
+                "src": "/venise-menu/pwa/icon-384x384.png",
                 "sizes": "384x384",
                 "type": "image/png"
               },
               {
-                "src": "/venise-menu/icons/icon-512x512.png",
+                "src": "/venise-menu/pwa/icon-512x512.png",
                 "sizes": "512x512",
                 "type": "image/png"
               }
